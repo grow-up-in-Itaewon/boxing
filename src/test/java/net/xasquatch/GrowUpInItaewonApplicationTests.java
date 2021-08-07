33e23d95c21a7794f8790084977ecbaead11fe2e
DESKTOP-FROCU5M\고현석 <bigfoot7774@gmail.com>
2021-08-07 12:46:23 +0900

    초기환경 설정

---
+++ src/test/java/net/xasquatch/GrowUpInItaewonApplicationTests.java
@@ -0,0 +1,13 @@
+package net.xasquatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowUpInItaewonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
